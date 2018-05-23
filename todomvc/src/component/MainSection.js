d6ac82798a8c55691f6f5efa44bcb0247e706b30
huihui890 <780156578@qq.com>
2018-05-23 18:15:02 +0800

    first commit

---
+++ todomvc/src/component/MainSection.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class MainSection extends Component {
+    render() {
+        return (
+            <span/>
+        )
+    }
+}
+
+export default MainSection;
